3f85838f4f51f4c39baff211e19a921667693cdc
DawnHavoc <sarnavadas2@gmail.com>
2023-11-13 12:10:53 +0530

    commit

--- parent/component/model_pusher.py
+++ parent/component/model_pusher.py
@@ -2,9 +2,6 @@ import os
 import pandas as pd
 import pathlib 
 
-from sklearn.metrics import accuracy_score,classification_report
-from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
-from sklearn.svm import SVC
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
@@ -13,30 +10,25 @@ import pickle
 from model_trainer import tune_model
 import importlib.util
 
-
 # Specify the absolute path to source_file.py
 source_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../constants/__init__.py'))
-
 
 # Use importlib to import source_file
 spec = importlib.util.spec_from_file_location("__init__", source_file_path)
 source_file = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(source_file)
-
-# def load_model():
-   
-
+ 
 def getfile():
     path=[]
-    for dirname, _, filenames in os.walk('D:/Projects/Spam-SMS-Detection'): #'Projects' is the folder name in which the required files are saved
+    for dirname, _, filenames in os.walk(source_file.ROOT_DIR): 
         for filename in filenames:
-            if(pathlib.Path(os.path.join(dirname, filename)).suffix =='.csv'):
+            if(pathlib.Path(os.path.join(dirname, filename)).suffix =='.'+source_file.TRAIN_SET.split('.')[1]):
                 path.append(os.path.join(dirname, filename))
    
    
     train_set_filename=""
     for filename in path:
-        if(os.path.basename(filename)=='train_processed.csv'): #filename with extension
+        if(os.path.basename(filename)==source_file.TRAIN_SET_PROCESSED_NAME): 
             train_set_filename=filename
     return train_set_filename
 
